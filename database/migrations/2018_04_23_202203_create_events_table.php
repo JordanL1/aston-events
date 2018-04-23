8ddc5340d2f6e49b7dd3bae770beadc8e2eb7015
Jordan Lees <leesja@aston.ac.uk>
2018-04-23 23:51:03 +0100

    Migrations to set up tables for models

---
+++ database/migrations/2018_04_23_202203_create_events_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateEventsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('events', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('title');
+            $table->string('description');
+            $table->string('location');
+            $table->string('category');
+            $table->dateTime('date-time');
+            $table->unsignedInteger('organiser-id');
+            $table->unsignedInteger('likes')->default(0);
+
+            $table->timestamps();
+
+            $table->foreign('organiser-id')->references('id')->on('users')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('events');
+    }
+}
--- database/migrations/2018_04_23_204507_create_event_images_table.php
+++ database/migrations/2018_04_23_204507_create_event_images_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateUsersTable extends Migration
+class CreateEventImagesTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,15 +13,14 @@ class CreateUsersTable extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('event_images', function (Blueprint $table) {
             $table->increments('id');
-            $table->string('name');
-            $table->string('email')->unique();
-          #  $table->boolean('isAdmin')->default(0);
-          #  $table->boolean('isOrganiser')->default(0);
-            $table->string('password');
-            $table->rememberToken();
+            $table->unsignedInteger('event-id');
+            $table->longText('filename');
+
             $table->timestamps();
+
+            $table->foreign('event-id')->references('id')->on('events')->onDelete('cascade');
         });
     }
 
@@ -32,6 +31,6 @@ class CreateUsersTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('event_images');
     }
 }
