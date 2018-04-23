7383d18328c17df53eaad54aa692f717393922f7
Jordan Lees <leesja@aston.ac.uk>
2018-04-24 00:06:36 +0100

    Default route to show all events

--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,8 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', 'DisplayController@showAll')->name('all');
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
